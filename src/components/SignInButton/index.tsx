168ef704ca02a1365dcd23dc06fa4fc98ed234ed
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-07-11 15:26:12 -0300

    configurando o index do botão

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -4,10 +4,23 @@ import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  return (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub />
-      Sign in with Github
-    </button>
-  );
+    const isUserLoggedIn = true;
+    return isUserLoggedIn ? (
+        <button
+            type="button"
+            className={styles.signInButton}>
+            <FaGithub color="#04d361" />
+            User Logged
+            <FiX color="#737380" className={styles.closeIcon} />
+        </button>
+    ) : (
+        <button
+            type="button"
+            className={styles.signInButton}>
+            <FaGithub
+                color="#eba417" />
+
+            Sign in with Github
+        </button>
+    );
 }
