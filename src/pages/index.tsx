e4f32341666a1e4cae1abf88310a23ffae82e300
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-07-13 19:32:42 -0300

    create-home-and-your-styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
-    <Head>
-      <title> Home | ig.news </title>
-    </Head>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>For $9.99 month</span>
+          </p>
+
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
