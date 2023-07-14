dcb1424d8ea6f52c2430ef870f7bd3b0702facb0
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-07-14 18:59:08 -0300

    update-version-and-stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import Head from 'next/head';
-import { SubscribeButton } from '../components/SubscribeButton';
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { GetServerSideProps } from "next";
+import styles from "./home.module.scss";
 
-import styles from './home.module.scss';
-
-
-export default function Home() {
+export default function Home(props) {
   return (
     <>
       <Head>
@@ -14,18 +13,27 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>🙏 Hey, welcome</span>
-          <h1>News about the <span>React</span> world.</h1>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
           <p>
             Get access to all the publications <br />
             <span>For $9.99 month</span>
           </p>
 
           <SubscribeButton />
-
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
-  )
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      nome: "Clara"
+    },
+  };
+};
