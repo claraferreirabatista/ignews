c1a3d143470c3e35131a03963a6cb66cb396a8e0
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-07-23 19:08:30 -0300

    update_api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import styles from "./home.module.scss";
+import { stripe } from "../services/stripe";
 
-export default function Home(props) {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,10 +26,10 @@ export default function Home(props) {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>For $9.99 month</span>
+            <span>For {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -30,10 +38,23 @@ export default function Home(props) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1NTtE6HVqb5dFEFL8ramjOAJ", {
+    expand: ["product"],
+  });
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
   return {
     props: {
-      nome: "Clara"
+      product,
     },
+
+    revalidate: 60 * 60 * 24,
   };
 };
